59591a37929d5a1cfbc066fc365bb6444f8f6f87
mironal <miro.gunp@gmail.com>
2012-10-07 17:40:44 +0900

    タブでなんかするやつ

---
+++ MyThirdApp/MyThirdApp/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  MyThirdApp
+//
+//  Created by mironal on 2012/10/07.
+//  Copyright (c) 2012年 org.logginging. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ MyThirdApp/MyThirdApp/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  MyThirdApp
+//
+//  Created by mironal on 2012/10/07.
+//  Copyright (c) 2012年 org.logginging. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
