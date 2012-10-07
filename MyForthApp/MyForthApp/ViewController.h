a74053f9e874f0bae9507f3fa6571a8245912956
mironal <miro.gunp@gmail.com>
2012-10-07 17:46:57 +0900

    ボタンを押した時のイベント

---
+++ MyForthApp/MyForthApp/ViewController.h
@@ -0,0 +1,16 @@
+//
+//  ViewController.h
+//  MyForthApp
+//
+//  Created by mironal on 2012/10/07.
+//  Copyright (c) 2012年 org.logginging. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+@property (weak, nonatomic) IBOutlet UILabel *labelText;
+
+- (IBAction)buttonPush:(id)sender;
+
+@end
---
+++ MyForthApp/MyForthApp/ViewController.m
@@ -0,0 +1,32 @@
+//
+//  ViewController.m
+//  MyForthApp
+//
+//  Created by mironal on 2012/10/07.
+//  Copyright (c) 2012年 org.logginging. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+- (IBAction)buttonPush:(id)sender {
+    self.labelText.text = @"Pushed me";
+}
+@end
